3008d9ad64ee40040abd02431f0c098a8ede1806
NJohnsonJ <njohnsonj@outlook.com>
2021-06-02 18:13:07 -0500

    Add Align story

--- stories/Align.stories.tsx
+++ stories/Align.stories.tsx
@@ -2,26 +2,26 @@ import React, {ComponentProps} from "react";
 import "uikit/dist/css/uikit.min.css";
 import "uikit/dist/js/uikit.min.js";
 import { Story } from "@storybook/react"
-import Alert from "../src/Alert";
+import Align from "../src/Align";
+
 
 export default {
-    title: "Alert",
-    component: Alert
+    title: "Align",
+    component: Align
 }
 
-const Template: Story<ComponentProps<typeof Alert>> = (args) => (
-    <Alert {...args}>
-        <Alert.Button />
-        <h1>Example Alert</h1>
+const Template: Story<ComponentProps<typeof Align>> = (args) => (
+    <div>
+        <Align {...args}>
+            <img width="450px" src="https://cdn.pixabay.com/photo/2020/12/14/11/18/cat-5830643_960_720.jpg"/>
+        </Align>
+        <h1>Flashy Title</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cras sed felis eget velit aliquet sagittis id consectetur purus. Aenean pharetra magna ac placerat. Aliquam malesuada bibendum arcu vitae elementum curabitur. Morbi tincidunt ornare massa eget egestas purus viverra accumsan. Nec ultrices dui sapien eget. Duis tristique sollicitudin nibh sit amet commodo nulla. Vitae turpis massa sed elementum tempus. Placerat vestibulum lectus mauris ultrices eros in cursus turpis. Pharetra vel turpis nunc eget lorem dolor sed. Massa enim nec dui nunc mattis enim.</p>
-    </Alert>
+    </div>
 )
 
 export const Example = Template.bind({});
 
 Example.args = {
-    animation: true,
-    duration: 200,
-    variant: undefined
+    alignment: "left"
 }
-
